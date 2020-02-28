d33461ab88b1e746cc1208b5609decec95411f7f
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-28 16:54:08 +0530

    models

---
+++ models/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://test_user:XgaG81tEsLOKJw00@mongocluster-vahm9.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+require('./student.model');
---
+++ models/student.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// create a schema
+const studentSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'Full Name is required'
+    },
+    email: {
+        type: String,
+        required: 'Email is required'
+    },
+    mobile: {
+        type: String,
+        required: 'Mobile is required'
+    },
+    city: {
+        type: String,
+        required: 'City is required'
+    }
+});
+
+// compile the schema into a model which gives a class
+mongoose.model('Student', studentSchema);
